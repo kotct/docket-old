b8037582de2f2cdd7ae88e125b2a89efebc750a6
Kristofer Rye <kristofer.rye@gmail.com>
2013-12-20 19:43:08 -0500

    Fixes and updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
 
+	before_filter :check_for_user_if_not_session_controller
+
 	def current_user
 		if session["current_user_id"]
 			User.find session["current_user_id"]
@@ -12,6 +14,10 @@ class ApplicationController < ActionController::Base
 	end
 
 	def check_for_user
-		redirect_to root_path, :alert => "You are not logged in!" unless current_user
+		redirect_to log_in_path, :alert => "You are not logged in!" unless current_user
+	end
+
+	def check_for_user_if_not_session_controller
+		check_for_user if params[:controller] != "sessions"
 	end
 end
--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,74 +1,76 @@
 class AssignmentsController < ApplicationController
-  before_action :set_assignment, only: [:show, :edit, :update, :destroy]
+	before_action :set_assignment, only: [:show, :edit, :update, :destroy]
 
-  # GET /assignments
-  # GET /assignments.json
-  def index
-    @assignments = Assignment.all
-  end
+	check_for_user
 
-  # GET /assignments/1
-  # GET /assignments/1.json
-  def show
-  end
+	# GET /assignments
+	# GET /assignments.json
+	def index
+		@assignments = Assignment.all
+	end
 
-  # GET /assignments/new
-  def new
-    @assignment = Assignment.new
-  end
+	# GET /assignments/1
+	# GET /assignments/1.json
+	def show
+	end
 
-  # GET /assignments/1/edit
-  def edit
-  end
+	# GET /assignments/new
+	def new
+		@assignment = Assignment.new
+	end
 
-  # POST /assignments
-  # POST /assignments.json
-  def create
-    @assignment = Assignment.new(assignment_params)
+	# GET /assignments/1/edit
+	def edit
+	end
 
-    respond_to do |format|
-      if @assignment.save
-        format.html { redirect_to @assignment, notice: 'Assignment was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @assignment }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @assignment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# POST /assignments
+	# POST /assignments.json
+	def create
+		@assignment = Assignment.new(assignment_params)
 
-  # PATCH/PUT /assignments/1
-  # PATCH/PUT /assignments/1.json
-  def update
-    respond_to do |format|
-      if @assignment.update(assignment_params)
-        format.html { redirect_to @assignment, notice: 'Assignment was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @assignment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @assignment.save
+				format.html { redirect_to @assignment, notice: 'Assignment was successfully created.' }
+				format.json { render action: 'show', status: :created, location: @assignment }
+			else
+				format.html { render action: 'new' }
+				format.json { render json: @assignment.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # DELETE /assignments/1
-  # DELETE /assignments/1.json
-  def destroy
-    @assignment.destroy
-    respond_to do |format|
-      format.html { redirect_to assignments_url }
-      format.json { head :no_content }
-    end
-  end
+	# PATCH/PUT /assignments/1
+	# PATCH/PUT /assignments/1.json
+	def update
+		respond_to do |format|
+			if @assignment.update(assignment_params)
+				format.html { redirect_to @assignment, notice: 'Assignment was successfully updated.' }
+				format.json { head :no_content }
+			else
+				format.html { render action: 'edit' }
+				format.json { render json: @assignment.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_assignment
-      @assignment = Assignment.find(params[:id])
-    end
+	# DELETE /assignments/1
+	# DELETE /assignments/1.json
+	def destroy
+		@assignment.destroy
+		respond_to do |format|
+			format.html { redirect_to assignments_url }
+			format.json { head :no_content }
+		end
+	end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def assignment_params
-      params.require(:assignment).permit(:name, :description, :due_date, :classroom_id)
-    end
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_assignment
+		@assignment = Assignment.find(params[:id])
+	end
+
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def assignment_params
+		params.require(:assignment).permit(:name, :description, :due_date, :classroom_id)
+	end
 end

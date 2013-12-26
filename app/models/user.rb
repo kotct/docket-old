333afbd4ded930f9dc9f2c464d4a1ee51d8e211c
Kristofer Rye <kristofer.rye@gmail.com>
2013-12-26 10:30:45 -0500

    Adding an email validation to the user model
    Not really necessary, but it helps prevent spam, I suppose.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,28 @@ class User < ActiveRecord::Base
 	validates :password, presence: true, on: :create
 	validates :email, presence: true, uniqueness: true
 	validates :name, presence: true
-	
+
+	validates_format_of :email, :with => /\A[\w\-\.]+\@[\w\-\.]+\z/
+
 	has_secure_password
 
 	def assignments
 		ret = []
+
 		self.classrooms.each do |c|
 			ret += c.assignments
 		end
+
 		ret
 	end
 
 	def exams
 		ret = []
+
 		self.classrooms.each do |c|
 			ret += c.exams
 		end
+
 		ret
 	end
 

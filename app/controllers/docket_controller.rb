6bd7af6ee47b423950be2e31b6a57ffda20af05a
Kristofer Rye <kristofer.rye@gmail.com>
2013-12-29 16:57:17 -0500

    Redoing Docket Info Generation

--- app/controllers/docket_controller.rb
+++ app/controllers/docket_controller.rb
@@ -2,35 +2,67 @@ class DocketController < ApplicationController
 	def index
 		redirect_to log_in_path, notice: "Please log in to use Docket." unless current_user
 
+		@current_day = Date.today
+
+		@days_of_today_and_tomorrow = []
+
+		(0..2).each do |wnum|
+			@days_of_today_and_tomorrow << @current_day + wnum.days
+		end
+
+		@days_of_this_week = []
+
+		difference_between_today_and_start_of_week = @current_day.wday
+
+		start_of_week = @current_day - difference_between_today_and_start_of_week.days
+
+		(0..6).each do |wnum|
+			@days_of_this_week << start_of_week + wnum.days
+		end
+
+		start_of_other_range = @days_of_this_week.last
+
 		@today_and_tomorrow_exams = []
+		@this_week_exams = []
 		@other_exams = []
+
 		current_user.exams.each do |e|
-			case 
-			when ((e.date == Date.today) or (e.date == Date.today + 1.day))
-				@today_and_tomorrow_exams << e
-			when e.date > Date.today + 1.day
+			case
+			when (@days_of_this_week.include? e.date)
+				@this_week_exams << e
+			when (@days_of_today_and_tomorrow.include? e.date)
+			      @today_and_tomorrow_exams << e
+			when (e.date > start_of_other_range)
 				@other_exams << e
 			end
 		end
 
 		@today_and_tomorrow_assignments = []
+		@this_week_assignments = []
 		@other_assignments = []
+
 		current_user.assignments.each do |e|
-			case 
-			when ((e.due_date == Date.today) or (e.due_date == Date.today + 1.day))
+			case
+			when (@days_of_this_week.include? e.due_date)
+				@this_week_assignments << e
+			when (@days_of_today_and_tomorrow.include? e.due_date)
 				@today_and_tomorrow_assignments << e
-			when e.due_date > Date.today + 1.day
+			when (e.due_date > start_of_other_range)
 				@other_assignments << e
 			end
 		end
 
 		@today_and_tomorrow_events = []
+		@this_week_events = []
 		@other_events = []
+
 		current_user.events.each do |e|
-			case 
-			when ((e.date == Date.today) or (e.date == Date.today + 1.day))
+			case
+			when (@days_of_this_week.include? e.date)
+				@this_week_events << e
+			when (@days_of_today_and_tomorrow.include? e.date)
 				@today_and_tomorrow_events << e
-			when e.date > Date.today + 1.day
+			when (e.date > start_of_other_range)
 				@other_events << e
 			end
 		end

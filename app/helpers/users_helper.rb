6264795b39776f1618ea9c574621156d288bcdbf
Sammidysam <sammidysam@gmail.com>
2013-12-29 23:06:42 -0500

    Separate exams and assignments

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+	def sort_by_period(list)
+		list.sort! { |first, last| first.period <=> last.period }
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,15 +28,15 @@ class User < ActiveRecord::Base
 	end
 
 	def day_assignments(day)
-		self.assignments.select {|assignment| assignment.due_date == day}
+		self.assignments.select { |assignment| assignment.due_date == day }
 	end
 
 	def day_exams(day)
-		self.exams.select {|exam| exam.date == day}
+		self.exams.select { |exam| exam.date == day }
 	end
 
 	def day_events(day)
-		self.events.select {|event| event.date > day.beginning_of_day && event.date < day.end_of_day}.sort! {|first,last| first.date <=> last.date}
+		self.events.select { |event| event.date > day.beginning_of_day && event.date < day.end_of_day}.sort! {|first,last| first.date <=> last.date }
 	end
 
 	def classes
@@ -44,7 +44,15 @@ class User < ActiveRecord::Base
 	end
 
 	def classes_with_activities_on(day)
-		self.classrooms.select {|classroom| classroom.day_activities(day).count > 0}
+		self.classrooms.select { |classroom| classroom.day_activities(day).count > 0 }
+	end
+
+	def classes_with_assignments_on(day)
+		self.classrooms.select { |classroom| classroom.day_assignments(day).count > 0 }
+	end
+
+	def classes_with_exams_on(day)
+		self.classrooms.select { |classroom| classroom.day_exams(day).count > 0 }
 	end
 
 	has_many :memberships

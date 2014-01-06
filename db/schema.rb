2c8c21dbc205986a5e175c288e1f058074d7e067
Kristofer Rye <kristofer.rye@gmail.com>
2014-01-06 10:11:16 -0500

    Update db schema to match new configuration migration
    Forgot to migrate, now I did.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131226143724) do
+ActiveRecord::Schema.define(version: 20140106150804) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 20131226143724) do
     t.integer  "teacher_id"
     t.text     "description"
     t.integer  "period"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "configurations", force: true do |t|
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

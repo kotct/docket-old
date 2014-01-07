7138965f08852d8dd6cfbaebbc9e3c3be715f7bd
Kristofer Rye <kristofer.rye@gmail.com>
2014-01-07 11:37:10 -0500

    Moar dynamicness!
    Woo, dynamicness \\o/!

--- app/assets/javascripts/docket-toggle.js
+++ app/assets/javascripts/docket-toggle.js
@@ -2,6 +2,10 @@ function docket_collapse_toggle(day_tag)
 {
 	var panel = document.getElementById("docket-collapsing-day-panel-" + day_tag);
 	var icon = panel.getElementsByClassName("docket-collapsing-indicator-image")[0];
+
+	var icon_class_string = icon.className;
+	var new_icon_class_string = "";
+
 	var body = panel.getElementsByClassName("docket-collapsing-div-data")[0];
 
 	var collapsed = panel.getAttribute("docket-is-collapsed") == "true" ? true : false;
@@ -9,12 +13,16 @@ function docket_collapse_toggle(day_tag)
 	if(collapsed) {
 		panel.setAttribute("docket-is-collapsed", "false");
 
-		icon.className = "docket-collapsing-indicator-image glyphicon pull-right glyphicon-chevron-up";
+		new_icon_class_string = icon_class_string.replace("glyphicon-chevron-down", "glyphicon-chevron-up");
+
+		icon.className = new_icon_class_string;
 		body.style.display = "inherit";
 	} else {
 		panel.setAttribute("docket-is-collapsed", "true");
 
-		icon.className = "docket-collapsing-indicator-image glyphicon pull-right glyphicon-chevron-down";
+		new_icon_class_string = icon_class_string.replace("glyphicon-chevron-up", "glyphicon-chevron-down");
+
+		icon.className = new_icon_class_string;
 		body.style.display = "none";
 	}
 }

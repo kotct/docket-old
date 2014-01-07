94085ef61c3d57abb92750ec6175f7fa51a0d83a
Kristofer Rye <kristofer.rye@gmail.com>
2014-01-07 12:06:10 -0500

    Moar+ Dynamicness!
    Arrays! Magic!

--- app/assets/javascripts/docket-toggle.js
+++ app/assets/javascripts/docket-toggle.js
@@ -2,27 +2,42 @@ function docket_collapse_toggle(day_tag)
 {
 	var panel = document.getElementById("docket-collapsing-day-panel-" + day_tag);
 	var icon = panel.getElementsByClassName("docket-collapsing-indicator-image")[0];
-
-	var icon_class_string = icon.className;
-	var new_icon_class_string = "";
-
 	var body = panel.getElementsByClassName("docket-collapsing-div-data")[0];
 
 	var collapsed = panel.getAttribute("docket-is-collapsed") == "true" ? true : false;
+	var icon_future_class_name = collapsed ? "glyphicon-chevron-up" : "glyphicon-chevron-down";
+
+	var icon_classes = icon.className.split(" ");
+
+	console.log(icon_classes);
+
+	var new_icon_classes = [];
+
+	icon_classes.map(function(cls)
+	{
+		var match = cls.match(/glyphicon-chevron-(up|down)/g);
+
+		/* Stop errors */
+		if(match == null || match == undefined) {
+			new_icon_classes.push(cls);
+
+			return;
+		}
+
+		if(match.length > 0) {
+			new_icon_classes.push(icon_future_class_name);
+
+			return;
+		}
+	});
+
+	icon.className = new_icon_classes.join(" ");
 
 	if(collapsed) {
 		panel.setAttribute("docket-is-collapsed", "false");
-
-		new_icon_class_string = icon_class_string.replace("glyphicon-chevron-down", "glyphicon-chevron-up");
-
-		icon.className = new_icon_class_string;
 		body.style.display = "inherit";
 	} else {
 		panel.setAttribute("docket-is-collapsed", "true");
-
-		new_icon_class_string = icon_class_string.replace("glyphicon-chevron-up", "glyphicon-chevron-down");
-
-		icon.className = new_icon_class_string;
 		body.style.display = "none";
 	}
 }

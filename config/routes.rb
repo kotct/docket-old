6eb4b5530e0ca5fead07c63852eadc7c998a9b2d
Kristofer Rye <kristofer.rye@gmail.com>
2014-01-18 06:30:10 -0500

    We don't need these comments anymore, either.

--- config/routes.rb
+++ config/routes.rb
@@ -35,10 +35,6 @@ Docket::Application.routes.draw do
 	get "log_in" => "sessions#new", :as => "log_in"
 	get "log_out" => "sessions#destroy", :as => "log_out"
 
-	# The priority is based upon order of creation: first created -> highest priority.
-	# See how all your routes lay out with "rake routes".
-
-	# You can have the root of your site routed with "root"
 	root "docket#index"
 
 	# Example of regular route:

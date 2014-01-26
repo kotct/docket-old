959d13b3246fef6893f03ab900001b0f0fe7edcf
Sammidysam <sammidysam@gmail.com>
2014-01-25 21:58:39 -0500

    Use helper functions in a non-stupid way

--- app/controllers/docket_controller.rb
+++ app/controllers/docket_controller.rb
@@ -1,12 +1,10 @@
 class DocketController < ApplicationController
-	include ApplicationHelper
-
 	skip_authorization_check
 
 	def index
 		redirect_to log_in_path, notice: "Please log in to use Docket." unless current_user
 
 		@current_day = Date.today
-		@days_of_this_week = get_day_array(@current_day, :week)
+		@days_of_this_week = view_context.get_day_array(@current_day, :week)
 	end
 end

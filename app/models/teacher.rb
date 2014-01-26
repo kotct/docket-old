e43399e20fd38d7f43571e4d530feb5affaaaf65
Kristofer Rye <kristofer.rye@gmail.com>
2014-01-26 13:16:03 -0500

    Name and Email Validations for Teachers
    Email shouldn't be validated for, but the name should. The email just
    needs to match a format.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,10 @@ class Teacher < ActiveRecord::Base
 	include PublicActivity::Model
 	tracked owner: -> (controller, model) { controller && controller.current_user }
 
+	validates :name, presence: true
+
+	validates_format_of :email, with: /\A[\w\-\.]+\@[\w\-\.]+\z/
+
 	def last_name
 		name.split.last
 	end
